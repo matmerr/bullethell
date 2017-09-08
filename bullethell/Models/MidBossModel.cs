f3adc43dbc2cbf783cb4065b209ffa9a02c76daf
Devon Honig <devon.honig12@gmail.com>
2017-09-08 09:55:38 -0700

    Added MiddleBoss class

---
+++ bullethell/Models/MidBossModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace bullethell.Models {
+    class MidBossModel : BaseModel{
+        private int health;
+        private double angle;
+        private double orbitRadius;
+
+        public Point OrbitPoint;
+
+        public MidBossModel(int startX, int startY, double startRate, Texture2D startSprite) : base(startX, startY, startRate, startSprite) {
+
+        }
+    }
+}
